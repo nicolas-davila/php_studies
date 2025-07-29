af2eb093a3842f87acd808729b6b51532d222eac
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-07-29 20:47:12 -0300

    Update index.php

--- ex003/index.php
+++ ex003/index.php
@@ -7,5 +7,12 @@
     //Teste de constantes
     const PAIS = "Brasil"; // Constantes não se usa "$"
 
-    echo "Olá! Meu nome é $nome $sobrenome e moro no ". PAIS;
+    //Exemplos usando camelCase e SNAKE_CASE
+    $minhaProfissao = "Analista de Sistemas";
+    $meu_salario = 2800.75;
+
+    const CURSO_REALIZADO = "Curso em Video de PHP moderno";
+    
+    echo "Olá! Meu nome é $nome $sobrenome e moro no ". PAIS; //Maneira correta de colocar uma constante no "echo".
+    echo "Minha profissão é $minhaProfissao, recebo R$$meu_salario e fiz o curso de " . CURSO_REALIZADO;
 ?>
