847edf1e2e2651cfb1e9096288666bf1e9f2fb1c
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-07-29 20:25:13 -0300

    Create index.php
    learning how to create variables and constants!

---
+++ ex003/index.php
@@ -0,0 +1,11 @@
+<?php 
+    $nome = "Nicolas";
+    $sobrenome = "Dávila";
+    //$nome = "Gabriel";
+    //$sobrenome = "Silva";
+
+    //Teste de constantes
+    const PAIS = "Brasil"; // Constantes não se usa "$"
+
+    echo "Olá! Meu nome é $nome $sobrenome e moro no ". PAIS;
+?>
