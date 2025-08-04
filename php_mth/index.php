b8a4f657d1e7cfa14c8dd33840a36aacac37a52e
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-04 12:17:18 -0300

    Aprendendo a somar e subtrair

--- php_mth/index.php
+++ php_mth/index.php
@@ -9,4 +9,12 @@
     $nomeCompleto = $nome . " " . $sobrenome; // Para concatenar utiliza-se o "." e as aspas "", para dar um espaçamento 
     echo "Olá, meu nome é $nome! <br>";
     echo "Meu nome completo é $nomeCompleto!";
+
+    //Somar e subtrair
+
+    $idade = 20;
+    $idadeSomada = $idade + 5;
+
+    echo "Minha idade é $idade <br>";
+    echo "minha idade somada é $idadeSomada";
 ?>
