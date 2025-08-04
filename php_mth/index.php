bf4e7420c43c2e04218504ab87d40d6900a608a2
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-04 12:03:17 -0300

    Aprendendo a concaternar

--- php_mth/index.php
+++ php_mth/index.php
@@ -1,4 +1,12 @@
 <?php 
     echo "Hello world! <br>"; // Para quebrar linha colocar a tag <br>
-    echo "Teste";
+    echo "Teste <br><br>";
+
+    //Para concatenar variáveis
+
+    $nome = "Nicolas";
+    $sobrenome = "Dávila";
+    $nomeCompleto = $nome . " " . $sobrenome; // Para concatenar utiliza-se o "." e as aspas "", para dar um espaçamento 
+    echo "Olá, meu nome é $nome! <br>";
+    echo "Meu nome completo é $nomeCompleto!";
 ?>
