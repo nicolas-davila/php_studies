1623e395432b3a41ed526ee66e5fda5c52b87208
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-07 10:56:13 -0300

    Utilizando funções aritméticas (abs())

---
+++ funcoes_aritmeticas/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+    //função abs retorna o valor absoluto de um número
+    $resultado = abs(500); //Sempre vai retornar um número positivo (sem o sinal)
+    
+    echo "O valo absoluto é: $resultado <br>";
+
+?>
