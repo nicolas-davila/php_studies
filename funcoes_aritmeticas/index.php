c7ce839d5ba3b6ff26900e7a1517f06f4bc2fbf8
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-07 11:10:30 -0300

    Utilizando funções aritméticas (min_e_max())

--- funcoes_aritmeticas/index.php
+++ funcoes_aritmeticas/index.php
@@ -9,5 +9,22 @@
     $resultado_convertido = base_convert(254, 10, 2);
     echo "O número 254 na base 2 é: $resultado_convertido <br>";
 
-    
+    //ceil arredonda um número para cima
+
+    //floor arredonda um número para baixo
+
+    //round arredonda um número para o inteiro mais próximo
+
+    //hypot calcula a hipotenusa de um triângulo retângulo
+
+    //intdiv divide dois números inteiros e retorna o quociente
+    $resultao_divisao = intdiv(5,2);
+    echo "A divisão inteira de 5 por 2 é: $resultao_divisao <br>";
+
+    //min retorna o menor valor entre os números fornecidos. max retorna o maior valor
+    $menor = min(10,5,20,15,25);
+    $maior = max(10,5,20,15,25);
+
+    echo "Maior valor é: $maior <br>";
+    echo "Menor valor é: $menor <br>";
 ?>
