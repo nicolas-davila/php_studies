508a704bd74697ae5e9ae8ff8fe47347a9bbd0c4
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-07 11:05:14 -0300

    Utilizando funções aritméticas (base_convert())

--- funcoes_aritmeticas/index.php
+++ funcoes_aritmeticas/index.php
@@ -3,6 +3,11 @@
     //função abs retorna o valor absoluto de um número
     $resultado = abs(500); //Sempre vai retornar um número positivo (sem o sinal)
     
-    echo "O valo absoluto é: $resultado <br>";
+    echo "O valor absoluto é: $resultado <br>";
 
+    //base_convert muda a base de um número
+    $resultado_convertido = base_convert(254, 10, 2);
+    echo "O número 254 na base 2 é: $resultado_convertido <br>";
+
+    
 ?>
