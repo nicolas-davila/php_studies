01c71a752dbf6cd3fb3f2f03df890a269f7e6645
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-07 11:56:43 -0300

    desafio de sortear número

---
+++ desafio02/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+    // Desafio 02 - Sortear um número entre 1 e 100
+    $numero_sorteado = rand(1, 100); // Função rand() gera um número aleatório entre os valores especificados
+    echo "O número sorteado é: $numero_sorteado";
+?>
