99649908bc52eff54db2d91e314635fee7daa280
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-07 11:54:49 -0300

    desafio de sucessor e antecessor de um número

---
+++ desafio01/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+    // Desafio 01 - Antecessor e Sucessor de um número
+    $numero = 10; // Exemplo de número
+    $antecessor = $numero - 1;
+    $sucessor = $numero +1; 
+
+    echo "O antecessor de $numero é $antecessor e o sucessor é $sucessor.";
+?>
