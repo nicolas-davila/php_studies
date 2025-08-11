5c87a9bd8a8fd1335b318a43f9645b8e86f00c35
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-11 09:22:04 -0300

    alterando tipo de random

--- desafio02/index.php
+++ desafio02/index.php
@@ -1,6 +1,13 @@
 <?php 
 
     // Desafio 02 - Sortear um número entre 1 e 100
-    $numero_sorteado = rand(1, 100); // Função rand() gera um número aleatório entre os valores especificados
+    // rand() Versão antiga e não recomendada para uso em aplicações críticas, por não trazertanta segurança quanto outras funções de geração de números aleatórios.
+    //outros tipos de random
+    //mt_rand() - Melhor performance e qualidade. Mais moderno e recomendada.
+    // random_int() - Gera um número aleatório de forma criptograficamente segura, recomendado para aplicações que necessitam de alta segurança. Mais lento que mt_rand, mas mais seguro.
+
+    //$numero_sorteado = rand(1, 100);
+    $numero_sorteado = mt_rand(0, 100);
+    //$numero_sorteado = random_int(1,100);
     echo "O número sorteado é: $numero_sorteado";
 ?>
